0b46d53d035e739e03ef5dbaa22d5c77ff774a4c
Tristan Ruel <tristanruel11@gmail.com>
2024-10-25 02:49:05 -0400

    Better cleaning

--- Data-preprocessing.py
+++ Data-preprocessing.py
@@ -7,13 +7,12 @@ yes = {'yes', 'y', 'YES', 'Yes', 'yEs', 'yeS'}
 if user_response.lower() in yes:
     file_path = 'Radiation Data/20241024_23_01_35.csv'
     data = pd.read_csv(file_path, header=None)
-
+    data.iloc[:, 3] = pd.to_numeric(data.iloc[:, 3], errors='coerce')
+    data = data[~data.iloc[:, 3].between(0, 1000, inclusive='both')]
     data = data.iloc[:, 4:]
-
     data = data.iloc[3:]
-
     data.to_csv(file_path, index=False, header=False)
 
-    print("First 4 columns and first 3 rows have been removed successfully.")
+    print("Rows with 4th column values between 0 and 1000 removed, and first 4 columns and first 3 rows have been removed successfully.")
 else:
     print("Ok")
